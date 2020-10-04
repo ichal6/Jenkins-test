85f2633821aee74c2199243324bd0440ae99caec
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-10-04 16:37:47 +0200

    Add method say

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(say());
+    }
+
+    public static String say(){
+        return "Hello world";
+    }
+}
